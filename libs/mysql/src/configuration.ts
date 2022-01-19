bf3cb9ef7175c33651c9300ddc30242d75173ed1
TuanCM <caomanhtuan@cryptopie-labo.com>
2022-01-19 16:28:35 +0700

    update config docker-compose.yml

--- libs/mysql/src/configuration.ts
+++ libs/mysql/src/configuration.ts
@@ -3,9 +3,9 @@ import { registerAs } from '@nestjs/config'
 export default registerAs('mysql', () => ({
   master: {
     host: process.env.REWARD_MYSQL_MASTER_HOST || 'localhost',
-    port: process.env.REWARD_MYSQL_MASTER_HOST || 3361,
-    user: process.env.REWARD_MYSQL_MASTER_HOST || 'root',
-    pass: process.env.REWARD_MYSQL_MASTER_HOST || 'password',
-    db: process.env.REWARD_MYSQL_MASTER_HOST || 'reward',
+    port: process.env.REWARD_MYSQL_MASTER_PORT || 3361,
+    user: process.env.REWARD_MYSQL_MASTER_USER || 'root',
+    pass: process.env.REWARD_MYSQL_MASTER_PASS || 'password',
+    db: process.env.REWARD_MYSQL_MASTER_DB || 'reward',
   },
 }))

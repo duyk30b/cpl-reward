3220d6a2a920543aa57a33fe230c673d2848e625
Tran Hoang Giang <tranhoanggiang5@gmail.com>
2022-03-29 00:37:53 +0700

    Fix link cashback

--- libs/external-cashback/src/external-cashback.service.ts
+++ libs/external-cashback/src/external-cashback.service.ts
@@ -29,7 +29,7 @@ export class ExternalCashbackService {
    */
   async changeUserCashback(input: ChangeUserCashback): Promise<any> {
     const postBoUrl =
-      this.configService.get('cashback.url') + '/balance/v1/transaction/create'
+      this.configService.get('cashback.url').replace('service-api', 'service-internal') + '/transaction/create'
 
     const postData = {
       ...input,

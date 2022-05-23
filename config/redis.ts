f4ccaec632636b8feb58dc6d00f0ab7ca8bc5864
Tan Nguyen Nhat <nguyennhattan@cryptopie-labo.com>
2022-05-23 11:15:57 +0700

    Fixed config name

--- config/redis.ts
+++ config/redis.ts
@@ -5,9 +5,9 @@ export default () => ({
     port: process.env.REWARD_REDIS_PORT || 6379,
     db: process.env.REWARD_REDIS_DB || 0,
     worker_limit_duration: process.env.REWARD_REDIS_WORKER_LIMIT_DURATION || 50,
-    banker_balance_duration:
+    banker_balance_limit_duration:
       process.env.REWARD_REDIS_BANKER_BALANCE_LIMIT_DURATION || 100,
-    banker_cashback_duration:
+    banker_cashback_limit_duration:
       process.env.REWARD_REDIS_BANKER_CASHBACK_LIMIT_DURATION || 200,
   },
 })

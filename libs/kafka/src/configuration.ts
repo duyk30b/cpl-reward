1a456a1bb05446fa4e574a1fe31e63f2872f9a47
Tran Hoang Giang <tranhoanggiang@cryptopie-labo.com>
2022-08-08 16:40:49 +0700

    Doi ten Kafka event

--- libs/kafka/src/configuration.ts
+++ libs/kafka/src/configuration.ts
@@ -4,38 +4,29 @@ export default registerAs('kafka', () => ({
   uri: process.env.KAFKA_URI,
   client: process.env.ENV + '-' + process.env.KAFKA_CONSUMER + '-client',
   consumer: process.env.ENV + '-' + process.env.KAFKA_CONSUMER,
-  auth_user_login: process.env.ENV + '_auth_user_login',
-  auth_user_change_email: process.env.ENV + '_auth_user_change_email',
-  auth_user_created: process.env.ENV + '_auth_user_created',
-  auth_user_logout: process.env.ENV + '_auth_user_logout',
-  auth_user_change_password: process.env.ENV + '_auth_user_change_password',
-  auth_user_change_info: process.env.ENV + '_auth_user_change_info',
+
+  auth_user_login: process.env.KAFKA_AUTH_USER_LOGIN,
+  auth_user_change_email: process.env.KAFKA_AUTH_USER_CHANGE_EMAIL,
+  auth_user_created: process.env.KAFKA_AUTH_USER_CREATED,
+  auth_user_logout: process.env.KAFKA_AUTH_USER_LOGOUT,
+  auth_user_change_password: process.env.KAFKA_AUTH_USER_CHANGE_PASSWORD,
+  auth_user_change_info: process.env.KAFKA_AUTH_USER_CHANGE_INFO,
   auth_user_authenticator_status_updated:
-    process.env.ENV + '_auth_user_authenticator_status_updated',
-  auth_user_kyc_status_updated:
-    process.env.ENV + '_auth_user_kyc_status_updated',
-  auth_user_kyc_registered: process.env.ENV + '_auth_user_kyc_registered',
+    process.env.KAFKA_AUTH_USER_AUTHENTICATOR_STATUS_UPDATED,
+  auth_user_kyc_status_updated: process.env.KAFKA_AUTH_USER_KYC_STATUS_UPDATED,
+  auth_user_kyc_registered: process.env.KAFKA_AUTH_USER_KYC_REGISTERED,
   auth_user_kyc_auto_kyc_finished:
-    process.env.ENV + '_auth_user_kyc_auto_kyc_finished',
-  auth_user_change_lv: process.env.ENV + '_auth_user_change_lv',
+    process.env.KAFKA_AUTH_USER_KYC_AUTO_KYC_FINISHED,
+  auth_user_change_lv: process.env.KAFKA_AUTH_USER_CHANGE_LV,
 
-  // BCE sử dụng
-  bce_trading_matched:
-    (process.env.ENV == 'prod-v2' ? 'production' : process.env.ENV) +
-    '_bce_trading_matched',
-  bce_deposit:
-    (process.env.ENV == 'prod-v2' ? 'production' : process.env.ENV) +
-    '_bce_deposit',
-  bce_withdraw:
-    (process.env.ENV == 'prod-v2' ? 'production' : process.env.ENV) +
-    '_bce_withdraw',
+  bce_trading_matched: process.env.KAFKA_BCE_TRADING_MATCHED,
+  bce_deposit: process.env.KAFKA_BCE_DEPOSIT,
+  bce_withdraw: process.env.KAFKA_BCE_WITHDRAW,
 
-  high_low_transfer_balance: process.env.ENV + '_high_low_transfer_balance',
-  high_low_create: process.env.ENV + '_high_low_create',
-  high_low_win: process.env.ENV + '_high_low_win',
-  high_low_lose: process.env.ENV + '_high_low_lose',
-  high_low_cancel: process.env.ENV + '_high_low_cancel',
-
-  // reward
-  reward_user_check_in: process.env.ENV + '_reward_user_check_in',
+  high_low_transfer_balance: process.env.KAFKA_HIGH_LOW_TRANSFER_BALANCE,
+  high_low_create: process.env.KAFKA_HIGH_LOW_CREATE,
+  high_low_win: process.env.KAFKA_HIGH_LOW_WIN,
+  high_low_lose: process.env.KAFKA_HIGH_LOW_LOSE,
+  high_low_cancel: process.env.KAFKA_HIGH_LOW_CANCEL,
+  reward_user_check_in: process.env.KAFKA_REWARD_USER_CHECK_IN,
 }))
